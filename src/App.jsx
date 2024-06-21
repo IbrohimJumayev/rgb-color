94f99ead82e25aa96f855efc5e45d39c5531ceca
Ibrohim Jumayev <ibrohimjumayev@MacBook-Air-Ibrohim.local>
2024-06-21 14:28:52 +0500

    last version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import React, { useReducer } from "react";
 
 const initialState = {
   red: 0,
@@ -7,84 +7,70 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
+  const newState = { red: state.red, green: state.green, blue: state.blue };
+
   switch (action.type) {
     case "INCREMENT_RED":
-      return { ...state, red: Math.min(state.red + 10, 255) };
+      newState.red = Math.min(state.red + 10, 255);
+      break;
     case "DECREMENT_RED":
-      return { ...state, red: (state.red - 10 + 256) % 256 };
+      newState.red = (state.red - 10 + 256) % 256;
+      break;
     case "INCREMENT_GREEN":
-      return { ...state, green: Math.min(state.green + 10, 255) };
+      newState.green = Math.min(state.green + 10, 255);
+      break;
     case "DECREMENT_GREEN":
-      return { ...state, green: (state.green - 10 + 256) % 256 };
+      newState.green = (state.green - 10 + 256) % 256;
+      break;
     case "INCREMENT_BLUE":
-      return { ...state, blue: Math.min(state.blue + 10, 255) };
+      newState.blue = Math.min(state.blue + 10, 255);
+      break;
     case "DECREMENT_BLUE":
-      return { ...state, blue: (state.blue - 10 + 256) % 256 };
+      newState.blue = (state.blue - 10 + 256) % 256;
+      break;
     default:
-      return state;
+      break;
   }
+
+  return newState;
 };
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const incrementRed = () => {
-    dispatch({ type: "INCREMENT_RED" });
-  };
-
-  const decrementRed = () => {
-    dispatch({ type: "DECREMENT_RED" });
-  };
-
-  const incrementGreen = () => {
-    dispatch({ type: "INCREMENT_GREEN" });
-  };
-
-  const decrementGreen = () => {
-    dispatch({ type: "DECREMENT_GREEN" });
-  };
-
-  const incrementBlue = () => {
-    dispatch({ type: "INCREMENT_BLUE" });
-  };
-
-  const decrementBlue = () => {
-    dispatch({ type: "DECREMENT_BLUE" });
-  };
-
-  const containerStyle = {
-    backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})`,
-    color: state.red + state.green + state.blue > 382 ? "black" : "white",
-    padding: "150px 50px",
-    textAlign: "center",
-  };
-
   return (
     <>
-      <div className="container" style={containerStyle}>
+      <div
+        className="container"
+        style={{
+          backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})`,
+        }}
+      >
         <h1>RGB Color: {`rgb(${state.red}, ${state.green}, ${state.blue})`}</h1>
       </div>
       <div className="colors">
-        <div className="red">
+        <div className="color-control">
           <p>Red: {state.red}</p>
-          <div className="buttons">
-            <button onClick={incrementRed}>+</button>
-            <button onClick={decrementRed}>-</button>
-          </div>
+          <button onClick={() => dispatch({ type: "INCREMENT_RED" })}>+</button>
+          <button onClick={() => dispatch({ type: "DECREMENT_RED" })}>-</button>
         </div>
-        <div className="green">
+        <div className="color-control">
           <p>Green: {state.green}</p>
-          <div className="buttons">
-            <button onClick={incrementGreen}>+</button>
-            <button onClick={decrementGreen}>-</button>
-          </div>
+          <button onClick={() => dispatch({ type: "INCREMENT_GREEN" })}>
+            +
+          </button>
+          <button onClick={() => dispatch({ type: "DECREMENT_GREEN" })}>
+            -
+          </button>
         </div>
-        <div className="blue">
+        <div className="color-control">
           <p>Blue: {state.blue}</p>
-          <div className="buttons">
-            <button onClick={incrementBlue}>+</button>
-            <button onClick={decrementBlue}>-</button>
-          </div>
+          <button onClick={() => dispatch({ type: "INCREMENT_BLUE" })}>
+            +
+          </button>
+          <button onClick={() => dispatch({ type: "DECREMENT_BLUE" })}>
+            -
+          </button>
         </div>
       </div>
     </>
